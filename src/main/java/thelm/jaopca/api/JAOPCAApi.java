a10a650756e32ced5c1e02433f931a384b9cefeb
TheLMiffy1111 <1111miffy@argentum.net>
2017-05-28 17:04:47 +0800

    1.0.10 changes
    Added setters in ItemEntry.

--- src/main/java/thelm/jaopca/api/JAOPCAApi.java
+++ src/main/java/thelm/jaopca/api/JAOPCAApi.java
@@ -8,7 +8,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.google.common.collect.HashBasedTable;
-import com.google.common.collect.HashMultimap;
+import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
@@ -23,71 +23,71 @@ import thelm.jaopca.api.item.ItemBase;
  * @author TheLMiffy1111
  */
 public class JAOPCAApi {
-	
+
 	/**
 	 * Contains all blocks registered by this mod, row = entry name, column = ore name
 	 */
 	public static final HashBasedTable<String,String,BlockBase> BLOCKS_TABLE = HashBasedTable.<String,String,BlockBase>create();
-	
+
 	/**
 	 * Contains all items registered by this mod, row = entry name, column = ore name
 	 */
 	public static final HashBasedTable<String,String,ItemBase> ITEMS_TABLE = HashBasedTable.<String,String,ItemBase>create();
-	
+
 	/**
 	 * Contains all fluids registered by this mod, row = entry name, column = ore name
 	 */
 	public static final HashBasedTable<String,String,FluidBase> FLUIDS_TABLE = HashBasedTable.<String,String,FluidBase>create();
-	
+
 	/**
 	 * List of all {@link ItemEntry}s.
 	 */
 	public static final ArrayList<ItemEntry> ITEM_ENTRY_LIST = Lists.<ItemEntry>newArrayList();
-	
+
 	/**
 	 * A map for convenience of all {@link ItemEntry}s.
 	 */
 	public static final HashMap<String,ItemEntry> NAME_TO_ITEM_ENTRY_MAP = Maps.<String,ItemEntry>newHashMap();
-	
+
 	/**
 	 * A map for convenience of all {@link ItemEntry}s.
 	 */
-	public static final HashMultimap<EnumEntryType,ItemEntry> TYPE_TO_ITEM_ENTRY_MAP = HashMultimap.<EnumEntryType,ItemEntry>create();
-	
+	public static final LinkedHashMultimap<EnumEntryType,ItemEntry> TYPE_TO_ITEM_ENTRY_MAP = LinkedHashMultimap.<EnumEntryType,ItemEntry>create();
+
 	/**
 	 * List of all {@link IOreEntry}s.
 	 */
 	public static final ArrayList<IOreEntry> ORE_ENTRY_LIST = Lists.<IOreEntry>newArrayList();
-	
+
 	/**
 	 * List of all {@link ModuleBase}s.
 	 */
 	public static final ArrayList<ModuleBase> MODULE_LIST = Lists.<ModuleBase>newArrayList();
-	
+
 	/**
 	 * A map for convenience of all {@link ModuleBase}s.
 	 */
 	public static final HashMap<String,ModuleBase> NAME_TO_MODULE_MAP = Maps.<String,ModuleBase>newHashMap();
-	
+
 	/**
 	 * A {@link ItemEntry} name to {@link ModuleBase} for convenience.
 	 */
-	public static final HashMultimap<String,IOreEntry> ENTRY_NAME_TO_ORES_MAP = HashMultimap.<String,IOreEntry>create();
-	
-	public static final HashMultimap<ModuleBase,IOreEntry> MODULE_TO_ORES_MAP = HashMultimap.<ModuleBase,IOreEntry>create();
-	
+	public static final LinkedHashMultimap<String,IOreEntry> ENTRY_NAME_TO_ORES_MAP = LinkedHashMultimap.<String,IOreEntry>create();
+
+	public static final LinkedHashMultimap<ModuleBase,IOreEntry> MODULE_TO_ORES_MAP = LinkedHashMultimap.<ModuleBase,IOreEntry>create();
+
 	/**
 	 * Set of textures to register
 	 */
 	public static final HashSet<ResourceLocation> TEXTURES = Sets.<ResourceLocation>newHashSet();
-	
+
 	/**
 	 * List of ore dict entry compats
 	 */
 	public static final ArrayList<ICompat> ORE_DICT_COMPAT_LIST = Lists.<ICompat>newArrayList();
-	
+
 	public static final Logger LOGGER = LogManager.getLogger("JAOPCA");
-	
+
 	/**
 	 * Register your {@link ModuleBase}s here.
 	 * @param module The module to register
@@ -96,7 +96,7 @@ public class JAOPCAApi {
 		MODULE_LIST.add(module);
 		NAME_TO_MODULE_MAP.put(module.getName(), module);
 	}
-	
+
 	public static boolean isModuleLoaded(String name) {
 		return NAME_TO_MODULE_MAP.containsKey(name);
 	}
