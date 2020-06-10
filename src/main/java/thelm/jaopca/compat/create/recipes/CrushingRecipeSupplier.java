f5d8cb65e84a6a35911c41f6acb4f2786dfab84a
TheLMiffy1111 <1111miffy@argentum.net>
2020-06-10 13:51:50 +0800

    Ported Create support

---
+++ src/main/java/thelm/jaopca/compat/create/recipes/CrushingRecipeSupplier.java
@@ -0,0 +1,73 @@
+package thelm.jaopca.compat.create.recipes;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.google.common.base.Strings;
+import com.simibubi.create.content.contraptions.components.crusher.CrushingRecipe;
+import com.simibubi.create.content.contraptions.processing.ProcessingIngredient;
+import com.simibubi.create.content.contraptions.processing.ProcessingOutput;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.ResourceLocation;
+import thelm.jaopca.utils.MiscHelper;
+
+public class CrushingRecipeSupplier implements Supplier<CrushingRecipe> {
+
+	private static final Logger LOGGER = LogManager.getLogger();
+
+	public final ResourceLocation key;
+	public final String group;
+	public final Object input;
+	public final Object[] output;
+	public final int time;
+
+	public CrushingRecipeSupplier(ResourceLocation key, Object input, int time, Object... output) {
+		this(key, "", input, time, output);
+	}
+
+	public CrushingRecipeSupplier(ResourceLocation key, String group, Object input, int time, Object... output) {
+		this.key = Objects.requireNonNull(key);
+		this.group = Strings.nullToEmpty(group);
+		this.input = input;
+		this.output = output;
+		this.time = time;
+	}
+
+	@Override
+	public CrushingRecipe get() {
+		Ingredient ing = MiscHelper.INSTANCE.getIngredient(input);
+		if(ing.hasNoMatchingItems()) {
+			throw new IllegalArgumentException("Empty ingredient in recipe "+key+": "+input);
+		}
+		List<ProcessingOutput> results = new ArrayList<>();
+		int i = 0;
+		while(i < output.length) {
+			Object out = output[i];
+			++i;
+			Integer count = 1;
+			if(i < output.length && output[i] instanceof Integer) {
+				count = (Integer)output[i];
+				++i;
+			}
+			Float chance = 1F;
+			if(i < output.length && output[i] instanceof Float) {
+				chance = (Float)output[i];
+				++i;
+			}
+			ItemStack stack = MiscHelper.INSTANCE.getItemStack(out, count);
+			if(stack.isEmpty()) {
+				LOGGER.warn("Empty output in recipe {}: {}", key, out);
+			}
+			results.add(new ProcessingOutput(stack, chance));
+		}
+		return new CrushingRecipe(key, group, Collections.singletonList(new ProcessingIngredient(ing)), results, time);
+	}
+}
---
+++ src/main/java/thelm/jaopca/compat/create/recipes/SplashingRecipeSupplier.java
@@ -0,0 +1,71 @@
+package thelm.jaopca.compat.create.recipes;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.google.common.base.Strings;
+import com.simibubi.create.content.contraptions.components.fan.SplashingRecipe;
+import com.simibubi.create.content.contraptions.processing.ProcessingIngredient;
+import com.simibubi.create.content.contraptions.processing.ProcessingOutput;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.ResourceLocation;
+import thelm.jaopca.utils.MiscHelper;
+
+public class SplashingRecipeSupplier implements Supplier<SplashingRecipe> {
+
+	private static final Logger LOGGER = LogManager.getLogger();
+
+	public final ResourceLocation key;
+	public final String group;
+	public final Object input;
+	public final Object[] output;
+
+	public SplashingRecipeSupplier(ResourceLocation key, Object input, Object... output) {
+		this(key, "", input, output);
+	}
+
+	public SplashingRecipeSupplier(ResourceLocation key, String group, Object input, Object... output) {
+		this.key = Objects.requireNonNull(key);
+		this.group = Strings.nullToEmpty(group);
+		this.input = input;
+		this.output = output;
+	}
+
+	@Override
+	public SplashingRecipe get() {
+		Ingredient ing = MiscHelper.INSTANCE.getIngredient(input);
+		if(ing.hasNoMatchingItems()) {
+			throw new IllegalArgumentException("Empty ingredient in recipe "+key+": "+input);
+		}
+		List<ProcessingOutput> results = new ArrayList<>();
+		int i = 0;
+		while(i < output.length) {
+			Object out = output[i];
+			++i;
+			Integer count = 1;
+			if(i < output.length && output[i] instanceof Integer) {
+				count = (Integer)output[i];
+				++i;
+			}
+			Float chance = 1F;
+			if(i < output.length && output[i] instanceof Float) {
+				chance = (Float)output[i];
+				++i;
+			}
+			ItemStack stack = MiscHelper.INSTANCE.getItemStack(out, count);
+			if(stack.isEmpty()) {
+				LOGGER.warn("Empty output in recipe {}: {}", key, out);
+			}
+			results.add(new ProcessingOutput(stack, chance));
+		}
+		return new SplashingRecipe(key, group, Collections.singletonList(new ProcessingIngredient(ing)), results, -1);
+	}
+}
