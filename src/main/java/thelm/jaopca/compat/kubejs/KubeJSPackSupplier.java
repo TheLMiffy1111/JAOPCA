3ff7703490080cc57e75c6ba5a5b475221569e80
unknown <TheLMiffy1111@users.noreply.github.com>
2023-09-22 01:25:39 +0800

    Updated KubeJS pack supplier

--- src/main/java/thelm/jaopca/compat/kubejs/KubeJSPackSupplier.java
+++ src/main/java/thelm/jaopca/compat/kubejs/KubeJSPackSupplier.java
@@ -1,9 +1,11 @@
 package thelm.jaopca.compat.kubejs;
 
+import java.io.File;
 import java.util.function.Consumer;
 
 import dev.latvian.mods.kubejs.KubeJSPaths;
-import net.minecraft.server.packs.FolderPackResources;
+import dev.latvian.mods.kubejs.script.data.KubeJSFolderPackResources;
+import net.minecraft.server.packs.FilePackResources;
 import net.minecraft.server.packs.PackResources;
 import thelm.jaopca.api.resources.IPackSupplier;
 import thelm.jaopca.api.resources.JAOPCAPackSupplier;
@@ -13,6 +15,11 @@ public class KubeJSPackSupplier implements IPackSupplier {
 
 	@Override
 	public void addPacks(Consumer<PackResources> resourcePacks) {
-		resourcePacks.accept(new FolderPackResources(KubeJSPaths.DIRECTORY.toFile()));
+		resourcePacks.accept(KubeJSFolderPackResources.PACK);
+		for(File file : KubeJSPaths.DATA.toFile().listFiles()) {
+			if(file.isFile() && file.getName().endsWith(".zip")) {
+				resourcePacks.accept(new FilePackResources(file));
+			}
+		}
 	}
 }
