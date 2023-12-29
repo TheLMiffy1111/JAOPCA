9df0d6bdf33ff26f0a179b0eaece97536fb83f94
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2023-12-30 02:49:43 +0800

    Update version number

--- src/main/java/thelm/jaopca/JAOPCA.java
+++ src/main/java/thelm/jaopca/JAOPCA.java
@@ -21,7 +21,7 @@ public class JAOPCA {
 
 	public static final String MOD_ID = "jaopca";
 	public static final String NAME = "JAOPCA";
-	public static final String VERSION = "1.12.2-2.3.8.13";
+	public static final String VERSION = "1.12.2-2.3.9.14";
 	public static final String DEPENDENCIES = "required-before:wrapup";
 	@Instance(JAOPCA.MOD_ID)
 	public static JAOPCA core;
