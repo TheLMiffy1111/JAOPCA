31cda850cc4146b666a46ea70af891795d8905c7
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2024-02-19 03:47:13 +0800

    Remove unnecessary mod instance

--- src/main/java/thelm/jaopca/JAOPCA.java
+++ src/main/java/thelm/jaopca/JAOPCA.java
@@ -23,8 +23,6 @@ public class JAOPCA {
 	public static final String NAME = "JAOPCA";
 	public static final String VERSION = "1.7.10-0@VERSION@";
 	public static final String DEPENDENCIES = "after:*";
-	@Instance(JAOPCA.MOD_ID)
-	public static JAOPCA core;
 	@SidedProxy(
 			clientSide = "thelm.jaopca.client.events.ClientEventHandler",
 			serverSide = "thelm.jaopca.events.CommonEventHandler",
