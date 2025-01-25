48ec744da14b4087c3106385950b04937703f1b7
TheLMiffy1111 <6522061+TheLMiffy1111@users.noreply.github.com>
2025-01-25 23:42:08 +0800

    Added Actually Subtractions support
    Fix possible issue in Thermal induction smelter recipe serialization
    Fix incorrect chances in Electrodynamics crystal crushing recipe

--- changelog.md
+++ changelog.md
@@ -1,1 +1,3 @@
-Readded Tinkers' Construct support  
+Added Actually Subtractions support  
+Fix possible issue in Thermal induction smelter recipe serialization  
+Fix incorrect chances in Electrodynamics crystal crushing recipe  
--- src/main/java/thelm/jaopca/compat/electrodynamics/ElectrodynamicsModule.java
+++ src/main/java/thelm/jaopca/compat/electrodynamics/ElectrodynamicsModule.java
@@ -128,7 +128,7 @@ public class ElectrodynamicsModule implements IModule {
 			}
 			helper.registerMineralCrusherRecipe(
 					new ResourceLocation("jaopca", "electrodynamics.crystal_to_impure_dust."+material.getName()),
-					crystalLocation, 1, impureDustInfo, 1, sulfurTrioxide, 1, 0, 0.1, 200, 450);
+					crystalLocation, 1, impureDustInfo, 1, sulfurTrioxide, 1, 0.05, 0, 200, 450);
 
 			helper.registerMineralGrinderRecipe(
 					new ResourceLocation("jaopca", "electrodynamics.impure_dust_to_dust."+material.getName()),
